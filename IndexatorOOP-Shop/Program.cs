e4e751a0920d91bdefcd66bcd23f158c194b1333
Denys Shaitan <schaitan.den@gmail.com>
2025-11-08 20:22:21 +0100

    Made a new code

--- IndexatorOOP-Shop/Program.cs
+++ IndexatorOOP-Shop/Program.cs
@@ -1,66 +1,119 @@
 ﻿using System;
+using System.Text;
 
 namespace IndexatorOOP_Shop
 {
     class Article
     {
-        private string product;
-        private string shop;
+
+
+        private string productname;
+        private string name;
         private double price;
 
-        public Article(string product, string shop, double price)
+        public Article(string productname, string name ,double price)
         {
-            this.product = product;
-            this.shop = shop;
+            this.productname = productname;
+            this.name = name;
             this.price = price;
         }
-
-        public string Product {  get { return product; } }
-        public string Shop {  get { return shop; } }
-        public double Price { get { return price; } }
+        public string Productname
+        {
+            get { return productname; }
+        }
+        public string Name
+        {
+            get { return name; }
+        }
+        public double Price
+        {
+            get { return price; }
+        }
 
         public void ShowInfo()
         {
-            Console.WriteLine("Product :" + product);
-            Console.WriteLine("Shop :" + shop);
-            Console.WriteLine("Price:" + price + "UAH");
+            Console.WriteLine("Назва товару:" + productname);
+            Console.WriteLine("Назва магазину:" + name);
+            Console.WriteLine("Ціна товару:" + price + "ГРН");
         }
+
     }
 
-    class Store 
+    class Store
     {
+        
+
         private Article[] articles;
 
-        public Store(Article[] goods)
+        public Store(Article[] articles)
         {
-            articles = goods;
+            this.articles = articles;
         }
 
         public Article this[int index]
         {
             get
             {
-                if (index >= 0 && index < articles.Length) 
+                if (index >= 0 && index < articles.Length)
                     return articles[index];
                 else
                     return null;
+                
             }
-            set
-            {
-                if (index >= 0 && index < articles.Length)
-                    articles[index] = value;
-                else
-                    Console.WriteLine("Спроба запису за межи масива");
-            }
-
         }
+
+        public void SearchByName(string productname)
+        {
+            bool found = false;
+
+            for (int i = 0; i < articles.Length; i++)
+            {
+                if (articles[i].Productname == productname)
+                {
+                    articles[i].ShowInfo();
+                    found = true;
+                    
+                }
+                    
+               
+ 
+            }
+            if (!found)
+                Console.WriteLine("Невідомий продукт");
+        }
+
     }
+
 
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.OutputEncoding = Encoding.Unicode;
+
+            Article[] articles = new Article[]
+            {
+                new Article("Печиво","АТБ",29.50),
+                new Article("Мандарин","Сільпо",49.99)
+            };
+
+            Store store = new Store(articles);
+
+
+
+            Console.WriteLine("Інформація товару за індексом:");
+            for(int i = 0;i < 2; i++)
+            {
+                Article article = store[i];
+                if (article != null)
+                    article.ShowInfo();
+            }
+
+            Console.WriteLine("Пошук товару за назвою");
+            string searchProduct = Console.ReadLine();
+            store.SearchByName(searchProduct);
+
+
         }
     }
 }
